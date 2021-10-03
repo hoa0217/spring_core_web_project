76efa76ed01e82642d6df4474653b32db2721feb
허정화 <gjwjdghk123@naver.com>
2021-10-04 00:23:47 +0900

    스프링 핵심 원리 예제 완료

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,19 +1,43 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
 
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository;
+
+    private final MemberRepository memberRepository; //final : 한번 지정해주면 못 바뀜, 초기값 또는 생성자에서만 값을 넣을 수 있음
     private final DiscountPolicy discountPolicy;
 
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+
+     //생성자 주입 -> 객체 생성 및 빈 등록 + 의존관계 주입
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
+
+    /*
+    @Autowired//수정자 주입 -> 의존관계 주입
+    public void setMemberRepository(MemberRepository memberRepository) {
+        System.out.println("set memberRepository = "+memberRepository);
+        this.memberRepository = memberRepository;
+    }
+
+    @Autowired
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        System.out.println("set discountPolicy = "+discountPolicy);
+        this.discountPolicy = discountPolicy;
+    }
+    */
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
@@ -21,5 +45,10 @@ public class OrderServiceImpl implements OrderService{
         int discountPrice = discountPolicy.discount(member, itemPrice);
 
         return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+
+    //테스트 용도
+    public MemberRepository getMemberRepository(){
+        return memberRepository;
     }
 }
